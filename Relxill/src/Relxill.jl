cca9146532cc517a445a89c72a3a9fc04e56f115
fjebaker <fergusbkr@gmail.com>
2025-01-20 16:51:16 +0000

    feat(Relxill): add XSPECModels dep

--- Relxill/src/Relxill.jl
+++ Relxill/src/Relxill.jl
@@ -4,12 +4,9 @@ using LibXSPEC_jll
 using LibXSPEC_Relxill_jll
 
 using SpectralFitting
-using SpectralFitting:
-    @xspecmodel,
-    @wrap_xspec_model_ccall,
-    AbstractSpectralModel,
-    Additive,
-    FitParam
+using SpectralFitting: AbstractSpectralModel, Additive, FitParam
+
+using XSPECModels: @xspecmodel, @wrap_xspec_model_ccall
 
 @xspecmodel (:lmodrelline, libXSPEC_relxill) struct XS_Relline{T} <:
                                                     AbstractSpectralModel{T,Additive}
@@ -39,15 +36,15 @@ end
 function XS_Relline(;
     K = FitParam(1.0),
     lineE = FitParam(6.4, upper_limit = 10.0),
-    index1 = FitParam(3.0, frozen=true),
-    index2 = FitParam(3.0, frozen=true),
-    r_break = FitParam(15.0, frozen=true),
-    a = FitParam(0.998, upper_limit = 0.998, frozen=true),
+    index1 = FitParam(3.0, frozen = true),
+    index2 = FitParam(3.0, frozen = true),
+    r_break = FitParam(15.0, frozen = true),
+    a = FitParam(0.998, upper_limit = 0.998, frozen = true),
     θ_obs = FitParam(30.0, upper_limit = 90.0),
-    inner_r = FitParam(-1.0, frozen=true),
-    outer_r = FitParam(400.0, frozen=true),
-    z = FitParam(0.0, frozen=true),
-    limb = FitParam(0.0, frozen=true),
+    inner_r = FitParam(-1.0, frozen = true),
+    outer_r = FitParam(400.0, frozen = true),
+    z = FitParam(0.0, frozen = true),
+    limb = FitParam(0.0, frozen = true),
 )
     XS_Relline(K, lineE, index1, index2, r_break, a, θ_obs, inner_r, outer_r, z, limb)
 end
@@ -85,25 +82,40 @@ end
 end
 function XS_Relxill(;
     K = FitParam(1.0),
-    index1 = FitParam(3.0, frozen=true),
-    index2 = FitParam(3.0, frozen=true),
-    r_break = FitParam(15.0, frozen=true),
-    a = FitParam(0.998, upper_limit = 0.998, frozen=true),
+    index1 = FitParam(3.0, frozen = true),
+    index2 = FitParam(3.0, frozen = true),
+    r_break = FitParam(15.0, frozen = true),
+    a = FitParam(0.998, upper_limit = 0.998, frozen = true),
     # theta_obs upper limit of 87 wasn't strictly obeyed resulting in
     # *** relxill error : incl 87.001  is not in the required range between 3-87 deg
     # this can also happen for the lower limit
     # *** relxill error : incl 3.000  is not in the required range between 3-87 deg
     θ_obs = FitParam(30.0, lower_limit = 4.0, upper_limit = 86.0),
-    inner_r = FitParam(-1.0, frozen=true),
-    outer_r = FitParam(400.0, frozen=true),
-    z = FitParam(0.0, frozen=true),
+    inner_r = FitParam(-1.0, frozen = true),
+    outer_r = FitParam(400.0, frozen = true),
+    z = FitParam(0.0, frozen = true),
     Gamma = FitParam(2.0, lower_limit = 1.0, upper_limit = 3.4),
-    logxi = FitParam(1.0, upper_limit = 4.7, frozen=true),
-    Afe = FitParam(1.0, frozen=true),
-    Ecut = FitParam(300.0, frozen=true),
-    refl_frac = FitParam(1.0)
+    logxi = FitParam(1.0, upper_limit = 4.7, frozen = true),
+    Afe = FitParam(1.0, frozen = true),
+    Ecut = FitParam(300.0, frozen = true),
+    refl_frac = FitParam(1.0),
 )
-    XS_Relxill(K, index1, index2, r_break, a, θ_obs, inner_r, outer_r, z, Gamma, logxi, Afe, Ecut, refl_frac)
+    XS_Relxill(
+        K,
+        index1,
+        index2,
+        r_break,
+        a,
+        θ_obs,
+        inner_r,
+        outer_r,
+        z,
+        Gamma,
+        logxi,
+        Afe,
+        Ecut,
+        refl_frac,
+    )
 end
 
 SpectralFitting.register_model_data(XS_Relxill, "xillver/xillver-a-Ec5.fits.gz")
