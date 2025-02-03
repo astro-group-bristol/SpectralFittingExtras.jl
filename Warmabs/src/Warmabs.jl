d01877dcea98f8806c52254acc96ece2385c642f
phajy <andy.young@bristol.ac.uk>
2025-02-03 11:47:22 +0000

    fix: warmabs

--- Warmabs/src/Warmabs.jl
+++ Warmabs/src/Warmabs.jl
@@ -4,8 +4,10 @@ using LibXSPEC_Warmabs_jll
 using LibXSPEC_Warmabs_jll: libXSPEC_fphotems
 
 using SpectralFitting
-using SpectralFitting:
-    AbstractSpectralModel, Multiplicative, FitParam, FreeParameters
+using SpectralFitting: AbstractSpectralModel, Additive, FitParam
+
+using XSPECModels
+using XSPECModels: @xspecmodel, @wrap_xspec_model_ccall
 
 @xspecmodel type = Float32 struct XS_WarmAbsorber{T,F} <:
                                   AbstractSpectralModel{T,Multiplicative}
